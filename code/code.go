0ceb04f65010705f2b6199edfcbd761c88d6d5f4
Eric Griffith <eric.griffith42@gmail.com>
2024-11-03 17:49:41 -0600

    implement Null to account for falsey conditionals

--- code/code.go
+++ code/code.go
@@ -25,6 +25,7 @@ const (
 	OpBang
 	OpJumpNotTruthy
 	OpJump
+	OpNull
 )
 
 type Definition struct {
@@ -48,6 +49,7 @@ var definitions = map[Opcode]*Definition{
 	OpBang:          {"OpBang", []int{}},
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
+	OpNull:          {"OpNull", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
