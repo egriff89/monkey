769b66672ee74f56090d308dcc9071c180caa86c
Eric Griffith <eric.griffith42@gmail.com>
2024-11-14 19:06:07 -0600

    Implement strings into compiler and VM

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -80,6 +80,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
+
 	case *ast.PrefixExpression:
 		err := c.Compile(node.Right)
 		if err != nil {
