bd4adf1fd90a516c28da4003fdd24e68e291c89c
Eric Griffith <eric.griffith42@gmail.com>
2024-11-27 14:00:36 -0600

    Implement compilation and execution of functions (#11)
    * added CompiledFunction object to represent functions
    
    * add new opcodes to handle function calls and returns
    
    * add TestFunctions compiler test and add new case branch to testConstants
    
    * added compiler scopes, compilation of function literals now possible
    
    * implement compilation of function calls
    
    * update VM to use stack frames for functions
    
    * implement OpCall, OpReturnValue, and OpReturn in VM to successfully execute functions from bytecode
    
    * Add test: TestFirstClassFunctions
    
    * add opcodes for local bindings, update `code.Make()` to work with uint8 opcodes
    
    * update `code.ReadOperands()` to work with one-byte operands
    
    * add test: TestLetStatementScopes
    
    * extend SymbolTable to handle local scope and bindings
    
    * extend compiler to resolve and compile local bindings
    
    * implement local bindings in VM
    
    * implement arguments in the compiler
    
    * implement arguments in the VM

--- object/object.go
+++ object/object.go
@@ -7,24 +7,26 @@ import (
 	"strings"
 
 	"github.com/egriff89/monkey/ast"
+	"github.com/egriff89/monkey/code"
 )
 
 type ObjectType string
 type BuiltinFunction func(args ...Object) Object
 
 const (
-	INTEGER_OBJ      = "INTEGER"
-	BOOLEAN_OBJ      = "BOOLEAN"
-	NULL_OBJ         = "NULL"
-	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
-	FUNCTION_OBJ     = "FUNCTION"
-	STRING_OBJ       = "STRING"
-	BUILTIN_OBJ      = "BUILTIN"
-	ARRAY_OBJ        = "ARRAY"
-	HASH_OBJ         = "HASH"
-	QUOTE_OBJ        = "QUOTE"
-	MACRO_OBJ        = "MACRO"
+	INTEGER_OBJ           = "INTEGER"
+	BOOLEAN_OBJ           = "BOOLEAN"
+	NULL_OBJ              = "NULL"
+	RETURN_VALUE_OBJ      = "RETURN_VALUE"
+	ERROR_OBJ             = "ERROR"
+	FUNCTION_OBJ          = "FUNCTION"
+	STRING_OBJ            = "STRING"
+	BUILTIN_OBJ           = "BUILTIN"
+	ARRAY_OBJ             = "ARRAY"
+	HASH_OBJ              = "HASH"
+	QUOTE_OBJ             = "QUOTE"
+	MACRO_OBJ             = "MACRO"
+	COMPILED_FUNCTION_OBJ = "COMPILED_FUNCTION_OBJ"
 )
 
 type Object interface {
@@ -212,3 +214,12 @@ func (m *Macro) Inspect() string {
 
 	return out.String()
 }
+
+type CompiledFunction struct {
+	Instructions  code.Instructions
+	NumLocals     int
+	NumParameters int
+}
+
+func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
+func (cf *CompiledFunction) Inspect() string  { return fmt.Sprintf("CompiledFunction[%p]", cf) }
