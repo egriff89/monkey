00f1a8dc108a57ca9f8525a5d25a1de7208c1529
Eric Griffith <eric.griffith42@gmail.com>
2024-12-27 20:04:52 -0600

    add OpGetFree opcode and new closure tests

--- code/code.go
+++ code/code.go
@@ -38,6 +38,7 @@ const (
 	OpSetLocal
 	OpGetBuiltin
 	OpClosure
+	OpGetFree
 )
 
 type Definition struct {
@@ -74,6 +75,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetLocal:      {"OpSetLocal", []int{1}},
 	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
 	OpClosure:       {"OpClosure", []int{2, 1}},
+	OpGetFree:       {"OpGetFree", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
